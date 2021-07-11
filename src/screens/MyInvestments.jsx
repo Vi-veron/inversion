1f96de6237b904c351caf4f19081b3a8160d80c8
Victoria Ogbiyoyo <vogbiyoyo@gmail.com>
2021-07-11 12:00:20 +0100

    feat: add MyInvestments screen

---
+++ src/screens/MyInvestments.jsx
@@ -0,0 +1,13 @@
+const { Typography } = require("@material-ui/core");
+
+function MyInvestments() {
+  return (
+    <div>
+      <Typography>
+        This page should contain a list of all the investments the user has
+        made.
+      </Typography>
+    </div>
+  );
+}
+export default MyInvestments;
